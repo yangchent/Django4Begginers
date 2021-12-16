7b5f17670cbcf6e318dd0c7f8da9045d7362880a
yangchen <gyachungriom@gmail.com>
2021-12-16 07:20:46 +0100

    seckey key hidden

--- blog/django_project/settings.py
+++ blog/django_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("django-insecure-@)4c_agq*0+4zi%b+6$i7p@x-r3m2#p6rp*sc@1)q$dlh(g&ir")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
