ae4817b423bbe1bbdce4286896971c6d3fce1b7d
yangchen <gyachungriom@gmail.com>
2021-12-30 13:24:14 +0100

    chapter 16 deployment steps

--- news/django_project/settings.py
+++ news/django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env # new
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+env = Env() # new
+env.read_env() # new
 
 # Application definition
 
